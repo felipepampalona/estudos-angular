611136871a9147b2dfdc859ebcda6c9a5dd35d7f
felipepampalona <felipepampalona@gmail.com>
2024-10-08 11:24:13 -0300

    adicionando modulos

---
+++ angular-components/src/app/components/menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'menu-component',
+  template: '<h2>Felipe</h2>',
+  styles: ['h2 { color: blueviolet}'],
+  standalone: true
+})
+
+export class MenuComponent {}
